4187008b1c9be5e3c73b1728190ed69c10df9460
VadosLight <it-vadim@inbox.ru>
2019-09-24 16:52:00 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Recource_lock_detection_Form
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.dataGridView2 = new System.Windows.Forms.DataGridView();
+			this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.btn_add_Res = new System.Windows.Forms.Button();
+			this.btn_del_Res = new System.Windows.Forms.Button();
+			this.btn_Remove_Row = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.BackgroundColor = System.Drawing.Color.Gray;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+			this.dataGridView1.Location = new System.Drawing.Point(12, 32);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersWidth = 30;
+			this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+			this.dataGridView1.Size = new System.Drawing.Size(486, 416);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// Column1
+			// 
+			this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+			this.Column1.HeaderText = "R1";
+			this.Column1.MaxInputLength = 3;
+			this.Column1.Name = "Column1";
+			this.Column1.Width = 46;
+			// 
+			// Column2
+			// 
+			this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+			this.Column2.HeaderText = "R2";
+			this.Column2.MaxInputLength = 3;
+			this.Column2.Name = "Column2";
+			this.Column2.Width = 46;
+			// 
+			// Column3
+			// 
+			this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+			this.Column3.HeaderText = "R3";
+			this.Column3.MaxInputLength = 3;
+			this.Column3.Name = "Column3";
+			this.Column3.Width = 46;
+			// 
+			// Column4
+			// 
+			this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+			this.Column4.HeaderText = "R4";
+			this.Column4.MaxInputLength = 3;
+			this.Column4.Name = "Column4";
+			this.Column4.Width = 46;
+			// 
+			// dataGridView2
+			// 
+			this.dataGridView2.AllowUserToAddRows = false;
+			this.dataGridView2.AllowUserToDeleteRows = false;
+			this.dataGridView2.BackgroundColor = System.Drawing.Color.Gray;
+			this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+			this.dataGridView2.Location = new System.Drawing.Point(502, 32);
+			this.dataGridView2.Name = "dataGridView2";
+			this.dataGridView2.RowHeadersWidth = 30;
+			this.dataGridView2.Size = new System.Drawing.Size(486, 62);
+			this.dataGridView2.TabIndex = 0;
+			// 
+			// dataGridViewTextBoxColumn1
+			// 
+			this.dataGridViewTextBoxColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+			this.dataGridViewTextBoxColumn1.HeaderText = "R1";
+			this.dataGridViewTextBoxColumn1.MaxInputLength = 3;
+			this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+			this.dataGridViewTextBoxColumn1.Width = 46;
+			// 
+			// dataGridViewTextBoxColumn2
+			// 
+			this.dataGridViewTextBoxColumn2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+			this.dataGridViewTextBoxColumn2.HeaderText = "R2";
+			this.dataGridViewTextBoxColumn2.MaxInputLength = 3;
+			this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+			this.dataGridViewTextBoxColumn2.Width = 46;
+			// 
+			// dataGridViewTextBoxColumn3
+			// 
+			this.dataGridViewTextBoxColumn3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+			this.dataGridViewTextBoxColumn3.HeaderText = "R3";
+			this.dataGridViewTextBoxColumn3.MaxInputLength = 3;
+			this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+			this.dataGridViewTextBoxColumn3.Width = 46;
+			// 
+			// dataGridViewTextBoxColumn4
+			// 
+			this.dataGridViewTextBoxColumn4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+			this.dataGridViewTextBoxColumn4.HeaderText = "R4";
+			this.dataGridViewTextBoxColumn4.MaxInputLength = 3;
+			this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+			this.dataGridViewTextBoxColumn4.Width = 46;
+			// 
+			// btn_add_Res
+			// 
+			this.btn_add_Res.BackColor = System.Drawing.Color.Transparent;
+			this.btn_add_Res.Location = new System.Drawing.Point(502, 100);
+			this.btn_add_Res.Name = "btn_add_Res";
+			this.btn_add_Res.Size = new System.Drawing.Size(222, 23);
+			this.btn_add_Res.TabIndex = 1;
+			this.btn_add_Res.Text = "Добавить ресурс";
+			this.btn_add_Res.UseVisualStyleBackColor = false;
+			this.btn_add_Res.Click += new System.EventHandler(this.btn_add_Res_Click);
+			// 
+			// btn_del_Res
+			// 
+			this.btn_del_Res.BackColor = System.Drawing.Color.Transparent;
+			this.btn_del_Res.Location = new System.Drawing.Point(502, 129);
+			this.btn_del_Res.Name = "btn_del_Res";
+			this.btn_del_Res.Size = new System.Drawing.Size(222, 23);
+			this.btn_del_Res.TabIndex = 1;
+			this.btn_del_Res.Text = "Удалить ресурс";
+			this.btn_del_Res.UseVisualStyleBackColor = false;
+			this.btn_del_Res.Click += new System.EventHandler(this.btn_del_Res_Click);
+			// 
+			// btn_Remove_Row
+			// 
+			this.btn_Remove_Row.BackColor = System.Drawing.Color.Transparent;
+			this.btn_Remove_Row.Location = new System.Drawing.Point(502, 157);
+			this.btn_Remove_Row.Name = "btn_Remove_Row";
+			this.btn_Remove_Row.Size = new System.Drawing.Size(221, 23);
+			this.btn_Remove_Row.TabIndex = 2;
+			this.btn_Remove_Row.Text = "Удалить строку";
+			this.btn_Remove_Row.UseVisualStyleBackColor = false;
+			this.btn_Remove_Row.Click += new System.EventHandler(this.btn_Remove_Row_Click);
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Transparent;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(730, 99);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(256, 81);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Поиск\r\nТупика\r\nРесурсов";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.richTextBox1.Font = new System.Drawing.Font("GOST Type AU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.richTextBox1.ForeColor = System.Drawing.Color.Lime;
+			this.richTextBox1.Location = new System.Drawing.Point(504, 193);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.ReadOnly = true;
+			this.richTextBox1.Size = new System.Drawing.Size(486, 253);
+			this.richTextBox1.TabIndex = 4;
+			this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.label2.Location = new System.Drawing.Point(502, 12);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(296, 16);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Доступные в начальный момент ресурсы";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(352, 16);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Таблица максимальных запрашиваемых ресурсов";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.ClientSize = new System.Drawing.Size(1000, 460);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.btn_Remove_Row);
+			this.Controls.Add(this.btn_del_Res);
+			this.Controls.Add(this.btn_add_Res);
+			this.Controls.Add(this.dataGridView2);
+			this.Controls.Add(this.dataGridView1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(1016, 499);
+			this.MinimumSize = new System.Drawing.Size(1016, 499);
+			this.Name = "Form1";
+			this.Text = "Определение тупиков ресурсов";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridView dataGridView2;
+		private System.Windows.Forms.Button btn_add_Res;
+		private System.Windows.Forms.Button btn_del_Res;
+		private System.Windows.Forms.Button btn_Remove_Row;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+		private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+		private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+		private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recource_lock_detection_Form
+{
+	public partial class Form1 : Form
+	{
+		uint[] available_Res;
+		uint[,] allocated_Res;
+		private int max_size_of_field = 3;
+		public Form1()
+		{
+			InitializeComponent();
+			dataGridView1.Rows.Add();
+			dataGridView2.Rows.Add();
+		}
+		private void btn_add_Res_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (dataGridView1.ColumnCount <= 9)
+				{
+					var column = new DataGridViewColumn(dataGridView1[0, 0]);
+					var column2 = new DataGridViewColumn(dataGridView2[0, 0]);
+					column.Width = 45;
+					column2.Width = 45;
+					
+					var column_count = dataGridView1.ColumnCount + 1;
+					column.HeaderText = "R" + column_count;
+					column2.HeaderText = "R" + column_count;
+					
+
+					dataGridView1.Columns.Add(column);
+					dataGridView2.Columns.Add(column2);
+
+					DataGridViewTextBoxColumn cName = (DataGridViewTextBoxColumn)dataGridView1.Columns[column_count-1];
+					cName.MaxInputLength = max_size_of_field;
+					DataGridViewTextBoxColumn cName2 = (DataGridViewTextBoxColumn)dataGridView2.Columns[column_count-1];
+					cName2.MaxInputLength = max_size_of_field;
+				}
+			}
+			catch (Exception)
+			{
+			//Ничего не делаем
+			}
+		}
+		private void btn_del_Res_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				var RowIndex = 0;
+				int ColumnIndex = dataGridView1.ColumnCount - 1;
+
+				dataGridView1.ClearSelection();
+				dataGridView2.ClearSelection();
+
+				dataGridView1.Rows[RowIndex].Cells[ColumnIndex].Selected = true;
+				dataGridView1.CurrentCell = dataGridView1.Rows[RowIndex].Cells[ColumnIndex];
+
+				dataGridView2.Rows[RowIndex].Cells[ColumnIndex].Selected = true;
+				dataGridView2.CurrentCell = dataGridView2.Rows[RowIndex].Cells[ColumnIndex];
+
+				if (dataGridView1.SelectedCells.Count != 0)
+					if (dataGridView1.SelectedCells[0].ColumnIndex > 2)
+					{
+						dataGridView1.Columns.RemoveAt(dataGridView1.SelectedCells[0].ColumnIndex);
+						dataGridView2.Columns.RemoveAt(dataGridView2.SelectedCells[0].ColumnIndex);
+					}
+			}
+			catch
+			{ }
+
+		}
+		private void btn_Remove_Row_Click(object sender, EventArgs e)
+		{
+			//Удаляем предпоследнюю строку в левой таблице
+			if (dataGridView1.RowCount >= 3)
+				dataGridView1.Rows.RemoveAt(dataGridView1.RowCount - 2); 
+		}
+
+		async void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				//Проверка левого поля на корректность данных (Буквы, пустые ячейки)
+				for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+					for (int j = 1; j < dataGridView1.ColumnCount; j++)
+					{
+						if (!uint.TryParse(dataGridView1.Rows[i].Cells[j].FormattedValue.ToString(), out uint d))
+						{
+							MessageBox.Show("Проверьте корректность введённых данных левого поля.");
+							return;
+						}
+					}
+				//Проверка правого поля на корректность данных (Буквы, пустые ячейки)
+				for (int i = 0; i < dataGridView2.RowCount; i++)
+					for (int j = 1; j < dataGridView2.ColumnCount; j++)
+					{
+						if (!uint.TryParse(dataGridView2.Rows[i].Cells[j].FormattedValue.ToString(), out uint d))
+						{
+							MessageBox.Show("Проверьте корректность введённых данных правого поля.");
+							return;
+						}
+					}
+				//Собираем данные из таблиц
+				available_Res = new uint[dataGridView2.ColumnCount];
+				allocated_Res = new uint[dataGridView1.RowCount - 1, dataGridView1.ColumnCount];
+				//Из левой таблицы
+				for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+				{
+					for (int j = 0; j < dataGridView1.ColumnCount ; j++)
+					{						
+						allocated_Res[i, j] = Convert.ToUInt16(dataGridView1.Rows[i].Cells[j].FormattedValue);
+						//MessageBox.Show("" + allocated_Res[i, j]);
+					}
+				}
+				//Из правой таблицы
+				for (int j = 0; j < dataGridView2.ColumnCount; j++)
+				{
+					available_Res[j] = Convert.ToUInt16(dataGridView2.Rows[0].Cells[j].FormattedValue);
+				}
+				//Поиск тупиков - тупик здесь только кодер
+				richTextBox1.Text = "";
+				bool flag = true;
+				uint count_of_good_steps = 0;
+				for (int step = 0; step < dataGridView1.RowCount - 1; step++)//Счётчик полных проходов по таблице
+				{					
+					for (int i = 0; i < dataGridView1.RowCount - 1; i++)//Счётчик, который указывает на текщую строку
+					{
+						for (int j = 0; j < dataGridView1.ColumnCount; j++)//Счётчик, который указывает на текущий элемент строки
+						{
+							if (allocated_Res[i, j] <= available_Res[j])
+							{
+								if (j == dataGridView1.ColumnCount - 1)
+								{
+									await Task.Delay(500);
+									++count_of_good_steps;
+									//Выводим логи для Пользователя
+									richTextBox1.AppendText("Высвобождаются ресурсы: ");
+
+									for(int l = 0; l < dataGridView1.ColumnCount; l++)
+										richTextBox1.AppendText("["+ allocated_Res[i,l] +"] ");
+									richTextBox1.AppendText("\n");
+
+									//Вершится магия
+									for (int l = 0; l < dataGridView1.ColumnCount; l++)
+									{
+										uint tmp1 = available_Res[l];
+										uint tmp2 = allocated_Res[i, l];
+
+										available_Res[l] = tmp1 + tmp2;
+										allocated_Res[i,l] = 99999999;
+									}
+
+									richTextBox1.AppendText("Доступные ресурсы: ");
+									for (int l = 0; l < dataGridView2.ColumnCount; l++)
+									{
+										richTextBox1.AppendText("[" + available_Res[l] + "] ");
+									}
+									richTextBox1.AppendText("\n");
+								}
+							}
+							else
+							{
+								break;
+							}
+						}
+					}
+				}
+				if (count_of_good_steps < (dataGridView1.RowCount - 1))
+				{
+					flag = false;
+
+					richTextBox1.AppendText("\nВозникла тупиковая ситуация на " + count_of_good_steps
+						+ "-м шаге");					
+				}
+				if (flag)
+				{
+					richTextBox1.AppendText("\nПоздравляем! Тупиковых ситуаций не обнаружено\n"
+						+ "Все процессы выполнены, ресурсы распределены.");
+				}
+			}
+			catch(Exception exc)
+			{
+				Console.WriteLine(exc.Message);				
+				//MessageBox.Show(exc.Message);
+			}
+		}
+	}
+}
